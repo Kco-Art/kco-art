c99aa4034d8376be3aa9e5e1cb62d0ff9e8cfb64
Benjamin Roth <benjamin@rubyist.fr>
2021-04-04 23:00:25 +0200

    first shot

---
+++ assets/javascript/application.js
@@ -0,0 +1,11 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+//     document.getElementById("navbar").style["margin-top"] = "0px";
+//     document.getElementById("kco").style["font-size"] = "2.25rem";
+//   } else {
+//     document.getElementById("navbar").style["margin-top"] = "2rem";
+//     document.getElementById("kco").style["font-size"] = "6.25rem";
+//   }
+// }
