483310833c5044677b246f96ad6a2a303b2ad7ff
Benjamin Roth <benjamin@rubyist.fr>
2021-04-04 23:24:47 +0200

    redimension header on scroll

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,11 +1,7 @@
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-//     document.getElementById("navbar").style["margin-top"] = "0px";
-//     document.getElementById("kco").style["font-size"] = "2.25rem";
-//   } else {
-//     document.getElementById("navbar").style["margin-top"] = "2rem";
-//     document.getElementById("kco").style["font-size"] = "6.25rem";
-//   }
-// }
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('#navbar, #kco').addClass('shrink');
+  } else {
+    $('#navbar, #kco').removeClass('shrink');
+  }
+});
