837cc020359d7800d2e62d54d675afac01ebbac3
Benjamin Roth <benjamin@rubyist.fr>
2021-04-11 20:25:53 +0200

    favicon + stop flickering

--- javascript/application.js
+++ javascript/application.js
@@ -1,7 +1,20 @@
 $(window).scroll(function() {
+
+  var screenHeight = window.innerHeight;
+  var pageHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+
+  if (screenHeight - 100 < pageHeight ) {
+    return
+  }
+
   if ($(document).scrollTop() > 200) {
     $('#navbar, #kco').addClass('shrink');
   } else {
     $('#navbar, #kco').removeClass('shrink');
   }
 });
+
